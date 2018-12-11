af22b52cb0039fefa04c2826f7abf6a1916470aa
王二 <648764190@qq.com>
2018-12-11 19:01:21 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+#app
+  .people-list
+    people(
+      v-for="n in 24"
+      :key="n"
+      :num="n - 1"
+      :direction="direction"
+    )
+  p
+    button(@click="direction = 'up'") 上
+    button(@click="direction = 'down'") 下
+    button(@click="direction = 'left'") 左
+    button(@click="direction = 'right'") 右
+</template>
+
+<script>
+import People from './components/people'
+export default {
+  name: 'App',
+  components: {
+    People
+  },
+  data () {
+    return {
+      direction: 'down'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+body {
+  background-color: #c3e9fd;
+}
+.people-list {
+  overflow: hidden;
+}
+</style>
---
+++ src/components/people.vue
@@ -0,0 +1,104 @@
+<template lang="pug">
+.people
+  .people-content(:style="peopleStyle") {{ num }}
+</template>
+
+<script>
+export default {
+  props: {
+    num: {
+      type: Number,
+      default: 0
+    },
+    speed: {
+      type: Number,
+      default: 500
+    },
+    direction: {
+      type: String,
+      default: 'down'
+    }
+  },
+  data () {
+    return {
+      step: 0
+    }
+  },
+  computed: {
+    peopleStyle () {
+      let style = {}
+      let { step, num, direction } = this
+      if (step === 3) step = 1
+      let x = 0
+      let y = 0
+      let imageIndex = ~~(num / 6) + 1
+      switch (num % 6) {
+        case 1:
+          x = 144
+          break
+        case 2:
+          x = 288
+          break
+        case 3:
+          y = 192
+          break
+        case 4:
+          y = 192
+          x = 144
+          break
+        case 5:
+          y = 192
+          x = 288
+          break
+      }
+      switch (direction) {
+        case 'up':
+          y += 144
+          break
+        case 'left':
+          y += 48
+          break
+        case 'right':
+          y += 96
+          break
+      }
+      style['background-position-x'] = -step * 48 - x + 'px'
+      style['background-position-y'] = -y + 'px'
+      style['background-image'] = `url(./static/images/people${imageIndex}.png)`
+      return style
+    }
+  },
+  mounted () {
+    this.loop()
+  },
+  methods: {
+    loop () {
+      this.timer && clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.addStep()
+        this.loop()
+      }, this.speed)
+    },
+    addStep () {
+      let { step } = this
+      step += 1
+      if (step >= 4) step = 0
+      this.step = step
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.people {
+  width: 48px;
+  height: 48px;
+  float: left;
+  // animation-iteration-count: 3;
+  .people-content {
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+  }
+}
+</style>
